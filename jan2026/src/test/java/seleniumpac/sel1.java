f5c561c8c6baf27cac42eb99534b8db3894cc7be
akanksha <sudeshchoudharyjalalabad@gmail.com>
2026-01-20 16:22:59 +0530

    20th work

---
+++ jan2026/src/test/java/seleniumpac/sel1.java
@@ -0,0 +1,25 @@
+package seleniumpac;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class sel1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriverManager.chromedriver().setup();
+WebDriver driver=new ChromeDriver();
+driver.get("https://www.google.com");
+WebElement search=driver.findElement(By.id("APjFqb"));
+search.sendKeys("Test methods");
+search.submit();
+System.out.println("title is"+driver.getTitle());
+driver.close();
+//WebElement searchbutton=driver.findElement(By.name("btnK"));
+	}
+
+}
